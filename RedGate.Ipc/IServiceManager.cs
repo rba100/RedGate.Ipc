d458f88b5ae25165402801cda13f9244c4f9d304
Robin Anderson <robin@rbanderson.co.uk>
2017-07-27 14:25:57 +0100

    add method to interface

--- RedGate.Ipc/IServiceManager.cs
+++ RedGate.Ipc/IServiceManager.cs
@@ -6,6 +6,12 @@ namespace RedGate.Ipc
     public interface IServiceManager
     {
         void Register<T>(object implementation);
+
+        /// <summary>
+        /// The delegateFactory will be called once per type per connection
+        /// and the result cached for that connection. It will not be disposed
+        /// when the connection is disposed.
+        /// </summary>
         void RegisterDi(Func<Type, object> delegateFactory);
 
         void AddEndpoint(IEndpoint endpoint);
