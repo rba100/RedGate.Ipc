0bd6514b512e41c412fdaaedb21e65e79037662f
Robin Anderson <robin@rbanderson.co.uk>
2017-08-09 09:48:15 +0100

    remove redundent declaration

--- RedGate.Ipc/Proxy/ProxyFactory.cs
+++ RedGate.Ipc/Proxy/ProxyFactory.cs
@@ -290,13 +290,4 @@ namespace RedGate.Ipc.Proxy
         object HandleCall(MethodInfo methodInfo, object[] args);
         void HandleDispose();
     }
-
-    /// <summary>
-    /// Can only be used on void methods.
-    /// When this is delclared on a method on interface declaration, calls to proxies of this interface
-    /// will return immediately after the request is sent, without waiting for the opertion to complete on the server side.
-    /// </summary>
-    public class ProxyNonBlockingAttribute : Attribute
-    {
-    }
 }
